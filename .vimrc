7f249a3a6f4209beb0480a93f3a21ea4ec1c9769
zuqq <noahheld17@gmail.com>
2023-09-22 23:24:09 +0100

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,38 @@
+noremap j gj
+noremap k gk
+
+syntax enable
+
+set autoindent
+set smartindent
+
+set autoread
+
+set backspace=indent,eol,start
+
+set clipboard=unnamed
+
+set expandtab
+set shiftwidth=4
+set softtabstop=4
+
+set formatoptions+=jn
+
+set hlsearch
+nnoremap <silent> <C-L> :nohlsearch<CR>
+set ignorecase
+set incsearch
+set smartcase
+
+set laststatus=2
+set ruler
+set scrolloff=0
+set wildmenu
+
+set nojoinspaces
+
+set splitbelow
+set splitright
+
+set ttimeout
+set ttimeoutlen=100
---
+++ home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }: {
+  home.username = "zuqq";
+  home.homeDirectory = "/Users/zuqq";
+
+  home.stateVersion = "23.05";
+
+  home.packages = [
+    pkgs.bash
+    pkgs.curl
+    pkgs.fd
+    pkgs.git
+    pkgs.hyperfine
+    pkgs.jq
+    pkgs.ripgrep
+    pkgs.tmux
+    pkgs.yq
+  ];
+
+  home.file = {
+    ".editrc".source = ./.editrc;
+
+    ".inputrc".source = ./.inputrc;
+
+    ".gitconfig".source = ./.gitconfig;
+    ".gitignore".source = ./.gitignore;
+
+    ".tmux.conf".source = ./.tmux.conf;
+
+    ".zsh" = {
+      source = ./.zsh;
+      recursive = true;
+    };
+    ".zshrc".source = ./.zshrc;
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-commentary
+    ];
+    extraConfig = builtins.readFile ./.vimrc;
+    packageConfigurable = pkgs.vim-darwin;
+  };
+}
