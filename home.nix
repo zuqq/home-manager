b5af88d494218328f6237308a0584f0291dc42e0
zuqq <noahheld17@gmail.com>
2023-09-24 13:02:51 +0100

    Move `programs.vim.extraConfig` into its own file

--- home.nix
+++ home.nix
@@ -47,28 +47,7 @@
 
       vim-commentary
     ];
-    extraConfig = ''
-      set autoindent
-      set smartindent
-
-      set expandtab
-      set shiftwidth=4
-      set softtabstop=4
-
-      set hlsearch
-      set ignorecase
-      set smartcase
-
-      set splitbelow
-      set splitright
-
-      noremap j gj
-      noremap k gk
-
-      set clipboard=unnamed
-
-      set nojoinspaces
-    '';
+    extraConfig = builtins.readFile ./.vimrc;
     packageConfigurable = pkgs.vim-darwin;
   };
 }
