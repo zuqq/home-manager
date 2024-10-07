f911de7aaf93a9e93f7fa1403f1393bbcb5af0b8
zuqq <noahheld17@gmail.com>
2024-10-07 12:43:35 +0100

    Hide WezTerm tab bar if there is only one tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,4 +3,5 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("SF Mono")
 config.font_size = 13.0
+config.hide_tab_bar_if_only_one_tab = true
 return config
